efda2de6926b08d77d6fd61109202209b72d3d32
lakshmi-ummidi <86336157+lakshmi-ummidi@users.noreply.github.com>
2024-04-10 09:00:04 +0530

    logical_op.v

--- logical_op.v
+++ logical_op.v
@@ -1,23 +1,14 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    05:00:33 04/02/2024 
-// Design Name: 
-// Module Name:    logical_op 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+/********************************************************************************************
+
+Filename    :	   logical_op.v  
+
+Description :      Design to verify functionality of logical operators
+
+Author Name :      Lakshmi Ummidi
+
+Version     :      1.0
+*********************************************************************************************/
 module logical_op();
 	reg [2:0]a, b;
 	reg x, y, z;
